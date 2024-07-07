0233da0de3edb1c43aef7f7821c3c211a4aea9d0
Square <aayushk4980@gmail.com>
2024-07-07 15:16:02 +0530

    Added intersection observer

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import '../styles/Contact.css';
 
 export default function Contact() {
 
+    // Reference to contact section.
+    const contactSectionRef = useRef(null);
+
+    // Intersection observer to track when contact section is on screen.
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            entries => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        // Changed body background and display contact section.
+                        document.body.style.backgroundColor = 'rgb(25, 199, 199)';
+                        contactSectionRef.current.style.opacity = '1';
+                    } else {
+                        // Changed body background and hide contact section.
+                        document.body.style.backgroundColor = 'transparent';
+                        contactSectionRef.current.style.opacity = '0';
+                    }
+                });
+            },
+            {
+                root: null,
+                rootMargin: '0px',
+                threshold: 0.5
+            }
+        );
+
+        // If contact section reference is not null, then observe contact section.
+        if (contactSectionRef.current) {
+            observer.observe(contactSectionRef.current);
+        }
+
+        // If this component unmounts, stop observing contact section.
+        return () => {
+            if (contactSectionRef.current) {
+                observer.unobserve(contactSectionRef.current);
+            }
+        };
+    }, []);
+
     return (
-        <section className="contact flex">
+        <section ref={contactSectionRef} className="contact flex">
             <h2>Contact Me:</h2>
             <div className="contactForm flex">
                 <form className="flex flex">
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import '../styles/Nav.css';
-import '../styles/Wave.css';
 
 export default function Nav() {
 
     return (
         <>
-        <div className="wave"></div>
         <nav className="nav flex">
             <li className="navLink">
                 <a href="#">Work</a>
