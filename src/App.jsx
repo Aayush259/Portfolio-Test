3d92c0e0d001d1f0388056d614ffc07d779a575e
Square <aayushk4980@gmail.com>
2024-07-08 16:31:54 +0530

    Added local storage for theme persistence

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,14 @@ const Footer = lazy(() => import('./components/Footer.jsx'));
 
 export default function App() {
 
-  // Initializing theme with light.
-  const [theme, setTheme] = useState('light');
+  // Getting theme from local storage if exists, otherwise initialize it with light theme.
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
 
-  // Update the theme by changing the body class.
+  // Update the theme by changing the body class and store the changed theme in local storage.
   const changeTheme = useCallback(() => {
     theme === 'light' ? document.body.classList.remove('dark') : document.body.classList.remove('light');
 
+    localStorage.setItem('theme', theme);
     document.body.classList.add(theme);
   }, [theme]);
 
