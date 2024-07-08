44225f328c315267107098ef949aedb6de30abcd
Square <aayushk4980@gmail.com>
2024-07-08 20:06:09 +0530

    Added FooterLinks.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,23 +3,39 @@ import linkedInImage from '../images/linkedin-logo.png';
 import gitHubImage from '../images/github-light.png';
 import xImage from '../images/x-twitter-light.svg';
 import '../styles/Footer.css';
+import FooterLinks from './FooterLinks.jsx';
 
 export default function Footer() {
 
+    const link1 = {
+        title: 'GitHub - Aayush259',
+        href: 'https://github.com/Aayush259',
+        imageSrc: gitHubImage,
+        imageAlt: 'GitHub'
+    };
+
+    const link2 = {
+        title: 'LinkedIn - Aayush Kumar Kumawat',
+        href: 'https://www.linkedin.com/in/aayush-kumar-kumawat/',
+        imageSrc: linkedInImage,
+        imageAlt: 'LinkedIn'
+    };
+
+    const link3 = {
+        title: 'X/Twitter - @Aayush259_',
+        href: 'https://x.com/Aayush259_',
+        imageSrc: xImage,
+        imageAlt: 'X/Twitter'
+    };
+
+    // Array of all links detail.
+    const linksArray = [link1, link2, link3];
+
     return (
         <footer className="footer flex">
-            <a href="https://github.com/Aayush259" className="footerLink flex">
-                <img src={gitHubImage} alt="GitHub" width={20} />
-                <span>GitHub - Aayush259</span>
-            </a>
-            <a href="https://www.linkedin.com/feed/" className="footerLink flex">
-                <img src={linkedInImage} alt="LinkedIn" width={20} />
-                <span>LinkedIn - Aayush Kumar Kumawat</span>
-            </a>
-            <a href="https://x.com/" className="footerLink flex">
-                <img src={xImage} alt="LinkedIn" width={20} />
-                <span>X-Twitter - @Aayush259_</span>
-            </a>
+            {
+                linksArray.map((link) => <FooterLinks key={link["title"]} linkDetails={link} />)
+            }
         </footer>
     );
 };
---
+++ src/components/FooterLinks.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function FooterLinks({ linkDetails }) {
+
+    // Getting link details from linkDetails prop.
+    const title = linkDetails['title'];
+    const href = linkDetails['href'];
+    const imageSrc = linkDetails['imageSrc'];
+    const imageAlt = linkDetails['imageAlt'];
+
+    return (
+        <a href={href} className="footerLink flex">
+            <img src={imageSrc} alt={imageAlt} width={20} />
+            <span>{title}</span>
+        </a>
+    );
+};
+
+FooterLinks.propTypes = {
+    linkDetails: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        href: PropTypes.string.isRequired,
+        imageSrc: PropTypes.node.isRequired,
+        imageAlt: PropTypes.string.isRequired
+    }).isRequired,
+};
