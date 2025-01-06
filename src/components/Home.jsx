b22e1d0997189ff16288b4a6368c6852ac64042b
Aayush <aayushk4980@gmail.com>
2025-01-06 22:42:35 +0530

    Minor Updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,47 +11,47 @@ export default function Home() {
     const workBtnImgRef = useRef(null);
     const connectBtnImgRef = useRef(null);
 
-  return (
-    <section className="home flex">
-        <div className="homeCard">
-            <p className="greeting">Hi! I'm</p>
-            <h1 className="myName">Aayush Kumar Kumawat</h1>
-            <p className="aboutMeHomeText flex">
-                and I
-                <span className="flex">
-                    <span>am a Frontend Developer!</span> 
-                    <span>am a Tech Enthusiast</span> 
-                    <span>make cool stuff 🧈</span> 
-                    <span>really like minimalism</span> 
-                    <span>like lots of sweets 🧁</span>
-                </span>
-            </p>
+    return (
+        <section className="home flex">
+            <div className="homeCard">
+                <p className="greeting">Hi! I'm</p>
+                <h1 className="myName">Aayush Kumar Kumawat</h1>
+                <p className="aboutMeHomeText flex">
+                    and I
+                    <span className="flex">
+                        <span>am a Full-stack Dev 🖥️</span>
+                        <span>am a Tech enthusiast 🚀</span>
+                        <span>make cool stuff 🧈</span>
+                        <span>like lots of sweets 🧁</span>
+                        <span>am shy but improving 😊</span>
+                    </span>
+                </p>
 
-            <div className="homeButtons flex">
-                <a href="https://github.com/Aayush259" target="_blank" aria-label='My Work'>
-                    <button
-                        className="gitHubBtn flex"
-                        onMouseEnter={() => {workBtnImgRef.current.src = cookieBiteLight}}
-                        onMouseLeave={() => {workBtnImgRef.current.src = cookieBiteDark}}
-                    >
-                        My Work
-                        <img ref={workBtnImgRef} src={cookieBiteDark} alt='Checkout my projects' height={20} width={20} />
-                    </button>
-                </a>
+                <div className="homeButtons flex">
+                    <a href="https://github.com/Aayush259" target="_blank" aria-label='My Work'>
+                        <button
+                            className="gitHubBtn flex"
+                            onMouseEnter={() => { workBtnImgRef.current.src = cookieBiteLight }}
+                            onMouseLeave={() => { workBtnImgRef.current.src = cookieBiteDark }}
+                        >
+                            My Work
+                            <img ref={workBtnImgRef} src={cookieBiteDark} alt='Checkout my projects' height={20} width={20} />
+                        </button>
+                    </a>
 
-                <a href="https://www.linkedin.com/in/aayush-kumar-kumawat/" target="_blank" aria-label='Connect with me'>
-                    <button
-                        className="linkedInBtn flex"
-                        onMouseEnter={() => {connectBtnImgRef.current.src = paperPlaneLight}}
-                        onMouseLeave={() => {connectBtnImgRef.current.src = paperPlaneDark}}
-                    >
-                        Got a job? Let's talk!
-                        <img ref={connectBtnImgRef} src={paperPlaneDark} alt='Connect with me' height={20} width={20} />
-                    </button>
-                </a>
+                    <a href="https://www.linkedin.com/in/aayush-kumar-kumawat/" target="_blank" aria-label='Connect with me'>
+                        <button
+                            className="linkedInBtn flex"
+                            onMouseEnter={() => { connectBtnImgRef.current.src = paperPlaneLight }}
+                            onMouseLeave={() => { connectBtnImgRef.current.src = paperPlaneDark }}
+                        >
+                            Got a job? Let's talk!
+                            <img ref={connectBtnImgRef} src={paperPlaneDark} alt='Connect with me' height={20} width={20} />
+                        </button>
+                    </a>
+                </div>
+
             </div>
-
-        </div>
-    </section>
-  );
+        </section>
+    );
 };
