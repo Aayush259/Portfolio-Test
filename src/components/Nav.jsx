3d3edd66cbd7f0748019ea924e00a137cc770188
Aayush259 <aayushk4980@gmail.com>
2025-02-08 16:55:19 +0530

    Fixed nav z-index

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -10,22 +10,20 @@ export default function Nav({ theme, setTheme }) {
     const themeIcon = theme === 'light' ? darkThemeIcon : whiteThemeIcon;
 
     return (
-        <>
-            <nav className="nav flex">
-                <button className="toggleThemeBtn" onClick={() => setTheme(prevTheme => prevTheme === 'light' ? 'dark' : 'light')} aria-label='Change Theme'>
-                    <img src={themeIcon} alt="ChangeTheme" height={20} width={20} />
-                </button>
-                <div className="navLink">
-                    <a href="#work" aria-label='Work'>Work</a>
-                </div>
-                <div className="navLink">
-                    <a href="#about" aria-label='About'>About</a>
-                </div>
-                <div className="navLink">
-                    <a href="#contact" aria-label='Connect'>Contact</a>
-                </div>
-            </nav>
-        </>
+        <nav className="nav flex">
+            <button className="toggleThemeBtn" onClick={() => setTheme(prevTheme => prevTheme === 'light' ? 'dark' : 'light')} aria-label='Change Theme'>
+                <img src={themeIcon} alt="ChangeTheme" height={20} width={20} />
+            </button>
+            <div className="navLink">
+                <a href="#work" aria-label='Work'>Work</a>
+            </div>
+            <div className="navLink">
+                <a href="#about" aria-label='About'>About</a>
+            </div>
+            <div className="navLink">
+                <a href="#contact" aria-label='Connect'>Contact</a>
+            </div>
+        </nav>
     );
 };
 
